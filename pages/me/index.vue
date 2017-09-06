9e877ddd417ee64ed68d33eb856dc804c24ba3fc
ash <ash@strategicdata.com.au>
2017-09-06 23:02:25 +1000

    Add me page

---
+++ pages/me/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="content">
+    <b-card>
+       ME!
+    </b-card>
+    </div>
+</template>
+<style>
+.card-body, .container, .card {
+    background-color: #333;
+}
+
+.content {
+    display: flex;
+    margin-top: 5em;
+    justify-content: center;
+}
+
+.card {
+    border: .25rem solid transparent;
+    border-color: rgba(255,255,255,.25);
+}
+</style>

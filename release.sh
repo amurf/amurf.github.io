67eeff5b1b56dab2e9b5d5e637ff8cda01caffd2
ash <ash@strategicdata.com.au>
2017-09-08 11:40:12 +1000

    Update release script to use docker surge

--- release.sh
+++ release.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-fig run site generate.sh
+# Comment this out if don't want to use docker image.
+function surge() {
+    docker run -it --rm -v "$PWD:$PWD" -w "$PWD" amurf/surge surge $@
+}
+
+docker-compose run site generate.sh
 surge dist
 
